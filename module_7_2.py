809930514eac8651d74992c9ad0aec909aabf237
mihamik603 <vmm1331@gmail.com>
2024-12-13 22:28:45 +0400

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -3,14 +3,14 @@ def custom_write(file_name, strings):
 
     with open(file_name, 'w', encoding='utf-8') as file:
         for index, string in enumerate(strings, start=1):
-            start_position = file.tell()  # Получаем текущую позицию в байтах
-            file.write(string + '\n')  # Записываем строку с переходом на новую строку
-            strings_positions[(index, start_position)] = string  # Сохраняем в словарь
+            start_position = file.tell() 
+            file.write(string + '\n') 
+            strings_positions[(index, start_position)] = string  
 
     return strings_positions
 
 
-# Пример выполняемого кода
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
